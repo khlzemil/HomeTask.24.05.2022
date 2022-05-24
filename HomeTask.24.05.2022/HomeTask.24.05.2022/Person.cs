f5210ca3c0005ad572e11b22bab9c348ad0a5cd8
khlzemil <emilmg@code.edu.az>
2022-05-24 15:09:42 +0400

    Added HomeTask

---
+++ HomeTask.24.05.2022/HomeTask.24.05.2022/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeTask._24._05._2022
+{
+    internal class Person
+    {
+        public string Name;
+        public string Surname;
+        public int Age;
+        public bool IsAdult;
+
+        public Person()
+        {
+            if(Age > 18)
+                IsAdult = true;
+            else
+                IsAdult = false;
+
+        }
+
+    }
+}
---
+++ HomeTask.24.05.2022/HomeTask.24.05.2022/Program.cs
@@ -0,0 +1,26 @@
+﻿using HomeTask._24._05._2022;
+using System;
+
+class Program
+{
+
+    public static void Main()
+    {
+
+        Person person1 = new Person
+        {
+            Name = "Emil",
+            Surname = "Quluzade",
+            Age = 19,
+        };
+
+        Console.WriteLine(person1.IsAdult);
+
+        
+
+
+    }
+
+
+
+}
